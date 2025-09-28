261bde8446d14c5de3181bc8795e2a35aa4cbd19
Amos Ombworo <amosombworo@gmail.com>
2025-09-28 23:51:04 +0300

    initial

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let modal = document.querySelector(".hidden");
+const likes = document.querySelectorAll(".like-glyph");
+
+likes.forEach((like) => {
+  like.addEventListener("click", (e) => {
+    if (like.innerText == EMPTY_HEART) {
+      like.innerText = FULL_HEART;
+    } else if (like.innerText == FULL_HEART) {
+      like.innerText = EMPTY_HEART;
+    }
+  });
+});
 
 
-
+mimicServerCall("http://mimicServer.example.com", {})
+  .then((promise) => alert(promise))
+  .catch((error) => {
+    modal.hidden = false;
+    modal.innerText += ` ${error.message}`;
+    setTimeout((modal.hidden = true), 3000);
+  });
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
